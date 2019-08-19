9e4eb61817514de8066170e91f14ff1c85c9add4
tblais1224 <thomasblais.student@careerdevs.com>
2019-08-19 14:02:36 -0400

    vars and consts looks good

--- HelloWorldMosh/Program.cs
+++ HelloWorldMosh/Program.cs
@@ -4,14 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorldMosh
+namespace Variables
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Tom");
-            Console.ReadLine();
+            byte number = 2;
+            int count = 10;
+            float totalPrice = 20.95f;
+            char character = 'a';
+            string firstName = "Tom";
+            bool isWorking = true;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(character);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
         }
     }
 }
