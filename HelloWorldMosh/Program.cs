feb1ac811cbb885967b4e9e596494c71b4bd6ba2
tblais1224 <thomasblais.student@careerdevs.com>
2019-08-20 12:21:10 -0400

    classes done

--- HelloWorldMosh/Program.cs
+++ HelloWorldMosh/Program.cs
@@ -4,28 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//use this to access the math folder 
+using HelloWorldMosh.Math;
+
 namespace Variables
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var a = 1;
-            var b = 2;
-            var c = 3;
-            // normal math order is followed, multiplication and division then add and subtract
-            //returns 7
-            Console.WriteLine(a + b * c);
-            //returns false
-            Console.WriteLine(a > b);
-            //returns true
-            Console.WriteLine(a != b);
-            //returns false
-            Console.WriteLine(!(a != b));
-            //returns false
-            Console.WriteLine(c > b && c == a);
-            // returns true
-            Console.WriteLine(c > b || c == a);
+            var john = new Person();
+            john.FirstName = "John";
+            john.LastName = "Smith";
+            john.Introduce();
+
+            Calculator calculator = new Calculator();
+            var result = calculator.Add(1, 2);
+            Console.WriteLine(result);
         }
     }
 }
