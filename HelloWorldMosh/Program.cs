d55ce9cf128cb8a68fc2b49c3c7526282235281f
tblais1224 <thomasblais.student@careerdevs.com>
2019-08-22 11:54:51 -0400

    ref and val types done

--- HelloWorldMosh/Program.cs
+++ HelloWorldMosh/Program.cs
@@ -6,36 +6,28 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Enums
+namespace RefAndValTypes
 {
-    public enum ShippingMethod
-    {
-        RegularAirMail = 1,
-        RegisteredAirMail = 2,
-        Express = 3
-    }
     class Program
     {
         static void Main(string[] args)
         {
-            var method = ShippingMethod.Express;
-            // returns 3
-            Console.WriteLine((int)method);
+            var a = 10;
+            // creates two memory locations in the stack
+            var b = a;
+            b++;
 
-            var methodId = 3;
-            //returns Express 
-            Console.WriteLine((ShippingMethod)methodId);
+            // returns a = 10 and b = 11
+            Console.WriteLine(string.Format("a: {0}, b: {1}", a, b));
 
-            //returns Express
-            Console.WriteLine(method.ToString());
+            var array1 = new int[3] { 1, 2, 3 };
+            // both arrays in the stack will point to same object in memory heap
+            var array2 = array1;
+            // this will change both arrays
+            array2[0] = 0;
+            // both 1 and 2 will return 0
+            Console.WriteLine(string.Format("1[0]: {0}, 2[0]: {1}", array1[0], array2[0]));
 
-            //returns Express
-            Console.WriteLine(method);
-
-            var methodName = "Express";
-            var shippingMethod = (ShippingMethod)Enum.Parse(typeof(ShippingMethod), methodName);
-            // return Express
-            Console.WriteLine(shippingMethod);
         }
     }
 }
