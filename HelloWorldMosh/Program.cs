90f37bc5a288bfa9d0300ba0c1eedd01049c81bb
tblais1224 <thomasblais.student@careerdevs.com>
2019-08-22 10:43:23 -0400

    finished arrays

--- HelloWorldMosh/Program.cs
+++ HelloWorldMosh/Program.cs
@@ -4,23 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-//use this to access the math folder 
-using HelloWorldMosh.Math;
 
-namespace Variables
+namespace Arrays
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var john = new Person();
-            john.FirstName = "John";
-            john.LastName = "Smith";
-            john.Introduce();
+            int[] numbers = new int[3];
+            numbers[0] = 1;
 
-            Calculator calculator = new Calculator();
-            var result = calculator.Add(1, 2);
-            Console.WriteLine(result);
+            Console.WriteLine(numbers[0]);
+            Console.WriteLine(numbers[1]);
+            Console.WriteLine(numbers[2]);
+
+            var flags = new bool[3];
+            flags[0] = true;
+
+            Console.WriteLine(flags[0]);
+            Console.WriteLine(flags[1]);
+            Console.WriteLine(flags[2]);
+
+            var names = new string[3] { "Tom", "Maddie", "Jill" };
+            Console.WriteLine(names[0]);
+            Console.WriteLine(names[1]);
+            Console.WriteLine(names[2]);
         }
     }
 }
