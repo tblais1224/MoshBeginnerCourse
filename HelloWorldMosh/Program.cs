b005b81c82d3f4ad1ede19a96be30f82a34631c2
tblais1224 <thomasblais.student@careerdevs.com>
2019-08-25 17:02:49 -0400

    datetime done

--- HelloWorldMosh/Program.cs
+++ HelloWorldMosh/Program.cs
@@ -22,7 +22,6 @@ namespace DateTime
             var tomorrow = now.AddDays(1);
             var yesterday = now.AddDays(-1);
 
-
             //Sunday, August 25, 2019
             Console.WriteLine(now.ToLongDateString());
             //8/25/2019
@@ -33,6 +32,32 @@ namespace DateTime
             Console.WriteLine(now.ToShortTimeString());
             //use formatter to get any desired format
             Console.WriteLine(now.ToString("yyyy-mm-dd"));
+
+
+            var timeSpan = new TimeSpan(1, 2, 3);
+
+            //the next two lines are the same 
+            var timeSpan1 = new TimeSpan(1, 0, 0);
+            var timeSpan2 = TimeSpan.FromHours(1);
+
+            var start = System.DateTime.Now;
+            var end = System.DateTime.Now.AddMinutes(12);
+            var duration = end - start;
+            Console.WriteLine("Duration: " + duration);
+
+            // properties
+            Console.WriteLine("Minutes: " + timeSpan.Minutes);
+            Console.WriteLine("Total Minutes: " + timeSpan.TotalMinutes);
+
+            //Add
+            Console.WriteLine("Add Example: " + timeSpan.Add(TimeSpan.FromMinutes(8)));
+            Console.WriteLine("Subtract Example: " + timeSpan.Subtract(TimeSpan.FromMinutes(3)));
+
+            //to string
+            Console.WriteLine("ToString: " + timeSpan.ToString());
+
+            //parse
+            Console.WriteLine("parse: " + TimeSpan.Parse("02:03:04"));
         }
     }
 }
