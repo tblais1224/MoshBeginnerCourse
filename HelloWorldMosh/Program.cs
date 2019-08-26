fd8142b1c4abc18818d8ca6dc24aca035ecbcbd2
tblais1224 <thomasblais.student@careerdevs.com>
2019-08-26 13:42:38 -0400

    completed debugging. all videos done in mosh begginner

--- HelloWorldMosh/Program.cs
+++ HelloWorldMosh/Program.cs
@@ -7,68 +7,62 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace FileInfoClasses
+namespace Debugging
 {
     class Program
     {
         static void Main(string[] args)
         {
-            File.Copy("c:\\temp\\myfile.jpg", "d:\\temp\\myfile.jpg", true);
-
-            //use @ to get rid of using double backslash
-
-            var path = @"c:\somefile.jpg";
-
-            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
-
-            File.Delete(path);
-            if (File.Exists(path))
+            //use defensive programming to catch bugs
+            var numbers = new List<int> { 1, 2, 3, 4, 5, 6 };
+            var smallest = GetSmallests(numbers, 3);
+            foreach (var num in smallest)
             {
-                //
+                Console.WriteLine(num);
+            }
+        }
+
+        public static List<int> GetSmallests(List<int> list, int count)
+        {
+            //catch a null list before executing code on it
+            if (list == null)
+            {
+                throw new ArgumentNullException("list");
             }
 
-            var content = File.ReadAllText(path);
-
-            var fileInfo = new FileInfo(path);
-            fileInfo.CopyTo("...");
-            fileInfo.Delete();
-            if (fileInfo.Exists)
+            //catch count error here, tell user where error is, and prevent app from going in the wrong state
+            if (count > list.Count || count <= 0)
             {
-                //
+                throw new ArgumentOutOfRangeException("count", "Count must be between 1 and the number of elements in the list");
             }
 
+            var buffer = new List<int>(list);
+            var smallests = new List<int>();
 
-
-            Directory.CreateDirectory(@"c:\temp\folder1");
-            var files = Directory.GetFiles(@"C:\Users\tomal\Documents\c#Code", "*.*", SearchOption.AllDirectories);
-            foreach (var file in files)
+            while (smallests.Count < count)
             {
-                Console.WriteLine(file);
+                var min = GetSmallest(buffer);
+                smallests.Add(min);
+                buffer.Remove(min);
             }
 
-            var directories = Directory.GetDirectories(@"C:\Users\tomal\Documents\c#Code", "*.*", SearchOption.AllDirectories);
-            foreach (var x in directories)
+            return smallests;
+        }
+
+        public static int GetSmallest(List<int> list)
+        {
+            //assume first num is smallest
+            var min = list[0];
+
+            for (int i = 1; i < list.Count; i++)
             {
-                Console.WriteLine(x);
+                if (list[i] < min)
+                {
+                    min = list[i];
+                }
             }
 
-            //            Directory.Exists("..put path here")
-
-
-            var directoryInfo = new DirectoryInfo("...");
-            directoryInfo.GetFiles();
-            directoryInfo.GetDirectories();
-
-
-
-            //path
-            var path = @"C:\Users\tomal\Documents\c#Code\CSharpTutuorials\Mosh\beginner\Classes.mp4";
-
-            Console.WriteLine("Extension: " + Path.GetExtension(path));
-            Console.WriteLine("File name: " + Path.GetFileName(path));
-            Console.WriteLine("File name without extension: " + Path.GetFileNameWithoutExtension(path));
-            Console.WriteLine("directory name: " + Path.GetDirectoryName(path));
-
+            return min;
         }
     }
 }
