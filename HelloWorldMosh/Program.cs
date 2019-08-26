c8999cbefde81d78ab8378a15807df7838899b55
tblais1224 <thomasblais.student@careerdevs.com>
2019-08-26 12:53:13 -0400

    finished filed, directory and path info classes

--- HelloWorldMosh/Program.cs
+++ HelloWorldMosh/Program.cs
@@ -13,41 +13,40 @@ namespace FileInfoClasses
     {
         static void Main(string[] args)
         {
-            //File.Copy("c:\\temp\\myfile.jpg", "d:\\temp\\myfile.jpg", true);
+            File.Copy("c:\\temp\\myfile.jpg", "d:\\temp\\myfile.jpg", true);
 
-            //us @ to get rid of using double backslash
-            //
-            //            var path = @"c:\somefile.jpg";
-            //
-            //            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
-            //
-            //            File.Delete(path);
-            //            if (File.Exists(path))
-            //            {
-            //                //
-            //            }
-            //
-            //            var content = File.ReadAllText(path);
-            //
-            //            var fileInfo = new FileInfo(path);
-            //            fileInfo.CopyTo("...");
-            //            fileInfo.Delete();
-            //            if (fileInfo.Exists)
-            //            {
-            //                //
-            //            }
+            //use @ to get rid of using double backslash
+
+            var path = @"c:\somefile.jpg";
+
+            File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+
+            File.Delete(path);
+            if (File.Exists(path))
+            {
+                //
+            }
+
+            var content = File.ReadAllText(path);
+
+            var fileInfo = new FileInfo(path);
+            fileInfo.CopyTo("...");
+            fileInfo.Delete();
+            if (fileInfo.Exists)
+            {
+                //
+            }
 
 
 
-            //            Directory.CreateDirectory(@"c:\temp\folder1");
-            //            var files = Directory.GetFiles(@"C:\Users\tomal\Documents\c#Code", "*.*", SearchOption.AllDirectories);
-            //            foreach (var file in files)
-            //            {
-            //                Console.WriteLine(file);
-            //            }
+            Directory.CreateDirectory(@"c:\temp\folder1");
+            var files = Directory.GetFiles(@"C:\Users\tomal\Documents\c#Code", "*.*", SearchOption.AllDirectories);
+            foreach (var file in files)
+            {
+                Console.WriteLine(file);
+            }
 
-            var directories =
-                Directory.GetDirectories(@"C:\Users\tomal\Documents\c#Code", "*.*", SearchOption.AllDirectories);
+            var directories = Directory.GetDirectories(@"C:\Users\tomal\Documents\c#Code", "*.*", SearchOption.AllDirectories);
             foreach (var x in directories)
             {
                 Console.WriteLine(x);
@@ -59,6 +58,17 @@ namespace FileInfoClasses
             var directoryInfo = new DirectoryInfo("...");
             directoryInfo.GetFiles();
             directoryInfo.GetDirectories();
+
+
+
+            //path
+            var path = @"C:\Users\tomal\Documents\c#Code\CSharpTutuorials\Mosh\beginner\Classes.mp4";
+
+            Console.WriteLine("Extension: " + Path.GetExtension(path));
+            Console.WriteLine("File name: " + Path.GetFileName(path));
+            Console.WriteLine("File name without extension: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("directory name: " + Path.GetDirectoryName(path));
+
         }
     }
 }
