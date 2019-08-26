2fa81409fe0967397f18e710dcd19e85c3fb5e59
tblais1224 <thomasblais.student@careerdevs.com>
2019-08-25 20:33:03 -0400

    summarizeTexts is done

--- HelloWorldMosh/Program.cs
+++ HelloWorldMosh/Program.cs
@@ -6,46 +6,15 @@ using System.Text;
 using System.Threading.Tasks;
 
 
-namespace Strings2
+namespace SummarizeTexts
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var fullName = "Tom Blais    ";
-            Console.WriteLine("Trim: '{0}'", fullName.Trim());
-            Console.WriteLine("Trim: '{0}'", fullName.Trim().ToUpper());
-
-            var index = fullName.IndexOf(' ');
-            var firstName = fullName.Substring(0, index);
-            var lastName = fullName.Substring(index + 1);
-            Console.WriteLine("fname: " + firstName);
-            Console.WriteLine("lname: " + lastName);
-
-
-            var names = fullName.Split(' ');
-            Console.WriteLine("Fname: " + names[0]);
-            Console.WriteLine("Lname: " + names[1]);
-
-
-            Console.WriteLine(fullName.Replace("Tom", "Maddie"));
-
-            if (String.IsNullOrWhiteSpace(" "))
-            {
-                Console.WriteLine("invalid");
-            }
-
-            var str = "25";
-            var age = Convert.ToByte(str);
-            Console.WriteLine(age);
-
-            float price = 29.95f;
-            //the C will convert to dollar sign
-            Console.WriteLine(price.ToString("C")
-            );
-            // round to whole dollars
-            Console.WriteLine(price.ToString("C0")
-            );
+            var sentence = "this is going to be a really long sentence to test different text options within c sharp";
+            var summary = StringUtility.SummarizeText(sentence, 30);
+            Console.WriteLine(summary);
         }
     }
 }
---
+++ HelloWorldMosh/StringUtility.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SummarizeTexts
+{
+    class StringUtility
+    {
+        public static string SummarizeText(string text, int maxLength = 20)
+        {
+            if (text.Length < maxLength)
+            {
+                return text;
+            }
+            else
+            {
+                var words = text.Split(' ');
+                var totalCharacters = 0;
+                var summaryWords = new List<string>();
+
+                foreach (var word in words)
+                {
+                    summaryWords.Add(word);
+                    totalCharacters += word.Length + 1;
+                    if (totalCharacters > maxLength)
+                    {
+                        break;
+                    }
+                }
+
+                return String.Join(" ", summaryWords) + "...";
+            }
+        }
+
+    }
+}
